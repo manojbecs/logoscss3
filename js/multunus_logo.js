3c7b6e2a23acddda803cbe62693cdc1a7436b215
Sreenath <sreenath.n@multunus.com>
2013-06-08 02:21:51 +0530

    implemented basic logo

--- js/multunus_logo.js
+++ js/multunus_logo.js
@@ -1,5 +1,38 @@
-(function() {
-	function addDotsToCharM(){
-		
+window.dotCount = {
+	"m" : 74,
+	"u" : 44,
+	"l" : 19,
+	"t" : 26,
+	"n" : 44,
+	"s" : 52
+};
+window.Dotted = {
+	addDotsToChar: function(letter, colorOrder) {
+		colorOrder = typeof colorOrder !== 'undefined' ? colorOrder : "primary";
+		var charElement = $("<div class='char'></div>");
+		var charClone = charElement.clone();
+		charClone.addClass(letter).addClass(colorOrder);
+		$(".dotted").append(charClone);
+		console.log("append letter: "+letter);
+		var dot = $("<div class='dot'></div>");
+		for(var index = 1; index < window.dotCount[letter]; index++) {
+			var dotClone = dot.clone();
+			dotClone.addClass("c"+index);
+			$(".dotted .char."+letter).append(dotClone);
+		}
+
+
+
 	}
-})();
+};
+
+$(document).ready(function(){
+	Dotted.addDotsToChar("m");
+	Dotted.addDotsToChar("u");
+	Dotted.addDotsToChar("l");
+	Dotted.addDotsToChar("t");
+	Dotted.addDotsToChar("u", "secondary");
+	Dotted.addDotsToChar("n");
+	Dotted.addDotsToChar("u", "secondary");
+	Dotted.addDotsToChar("s", "secondary");
+});
